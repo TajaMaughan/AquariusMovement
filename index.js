9debd1274414b4a1815fb27a8f287fa2c6320fb6
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-06-24 13:54:16 -0600

    started database layout

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = process.env.PORT ||3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use( express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});

f5ff5bcc4feb71b8e551cf6f653fadac5148ad53
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-06-24 18:16:27 -0600

    adding proxy back

--- index.js
+++ index.js
@@ -4,20 +4,10 @@ const db = require('./models');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use( express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  var mysql = require('mysql2');
-  var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-  connection.connect();
-} else {
-  db.sequelize.sync({ force: false }).then(function() {
-    app.listen(PORT, function() {
-      console.log('Aquarius Movement listening on PORT ' + PORT);
-    });
-  });
 }
 
 const routes = require('./routes/postRoutes');
@@ -26,3 +16,9 @@ app.use('/', routes);
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
+
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("Aquarius Movement listening on PORT " + PORT);
+  });
+});
